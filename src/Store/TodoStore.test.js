7414b2850cba23c3f27c0633532a24e1a6ffe52e
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-01 18:27:04 +0530

    [Red State] delete todo

--- src/Store/TodoStore.test.js
+++ src/Store/TodoStore.test.js
@@ -13,6 +13,12 @@ describe('TodoStore functionality ',() =>{
        expect(todoStore.todos[0].text).toBe("test driven development")
         expect(todoStore.todos[0].isCompleted).not.toBe(true)   
     })
-  
+  it('delete a todo',()=>{
+      const todoStore=new TodoStore()
+      expect(todoStore.todos.length).toBe(0)
+      todoStore.addTodo("tdd")
+      todoStore.deleteTodo(1)
+      expect(todoStore.todos.length).toBe(0)
+  })
   
 })
