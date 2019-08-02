de6ac6036925d437e585470c30fc2560389e1dfc
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-02 15:59:46 +0530

    [red state] for EachTodo component delete button

--- src/Components/TodoApp/TodoList/EachTodo/EachTodo.test.js
+++ src/Components/TodoApp/TodoList/EachTodo/EachTodo.test.js
@@ -2,7 +2,7 @@ import {render,cleanup, fireEvent} from '@testing-library/react'
 import React from 'react'
 import EachTodo from './index'
 import Todo from '../../../../Models/Todo'
-
+import TodoStore from '../../../../Store/TodoStore'
 afterEach(cleanup)
 describe('Test for EachTodo Component',()=>{
     it('should check the checkbox',()=>{
@@ -12,7 +12,16 @@ describe('Test for EachTodo Component',()=>{
         const todoCompleted=getByTestId("checkbox")
         fireEvent.click(todoCompleted)
         expect(todo.toggleTodo).toHaveBeenCalled()
-        
+    })
+    it('should delete a todo from list of todos',()=>{
+        const todoStore=new TodoStore()
+        jest.spyOn(todoStore,'deleteTodo')
+        todoStore.addTodo("react")
+        const {getByTestId}=render(<EachTodo todoStore={todoStore}/>)
+        const deleteTodo=getByTestId("delete-todo")
+        fireEvent.click(deleteTodo)
+        expect(todoStore.deleteTodo).toHaveBeenCalled()
+
 
     })
 })
--- src/Components/TodoApp/TodoList/EachTodo/index.js
+++ src/Components/TodoApp/TodoList/EachTodo/index.js
@@ -3,10 +3,14 @@ class EachTodo extends Component{
     handleClick=()=>{
         this.props.todo.toggleTodo()
     }
+    handleDelete=()=>{
+
+    }
 render(){
     return(
         <>
         <input data-testid="checkbox" type="checkbox" onClick={this.handleClick} />
+        <button data-testid="delete-todo" onClick={this.handleDelete}/>
         </>
     )
 }
