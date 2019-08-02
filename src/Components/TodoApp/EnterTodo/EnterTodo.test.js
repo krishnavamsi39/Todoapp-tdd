0196418b0871ae239304c6a261d3f84813f265eb
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-02 12:34:47 +0530

    [refactored state] for entertodo component

--- src/Components/TodoApp/EnterTodo/EnterTodo.test.js
+++ src/Components/TodoApp/EnterTodo/EnterTodo.test.js
@@ -1,10 +1,30 @@
-import {render,cleanup} from '@testing-library/react'
+import {render,cleanup, fireEvent} from '@testing-library/react'
 import React from 'react'
 import EnterTodo from './index'
-
+afterEach(cleanup)
 describe('test for EnterTodo Component',()=>{
     it('should check input box',()=>{
     const {getByPlaceholderText}=render(<EnterTodo />)
     const todoText=getByPlaceholderText("what needs to be done")
+    expect(todoText).toBeDefined()
+
     })
+    it('should check todo text value change and submit ',()=>{
+        const onKeyPressEnter=jest.fn()
+        const {getByPlaceholderText}=render(<EnterTodo onKeyPressEnter={onKeyPressEnter}/>)
+    const todoText=getByPlaceholderText("what needs to be done")
+        const changeEvent={
+            target:{
+                value:"react"
+            }
+        }
+        fireEvent.change(todoText,changeEvent)
+        const keyDownEvent={
+            key:"Enter",
+            code:13,
+            keyCode:13
+        }
+        fireEvent.keyDown(todoText,keyDownEvent)
+        expect(onKeyPressEnter).toHaveBeenCalledWith("react")
+        })
 })
--- src/Components/TodoApp/EnterTodo/index.js
+++ src/Components/TodoApp/EnterTodo/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 class EnterTodo extends Component{
+    constructor(props){
+        super(props)
+        this.state={todoText:""}
+    }
+    handleChange=(e)=>{
+        this.setState({todoText:e.target.value})
+    }
+    handleKeyDown=(e)=>{
+        if(e.keyCode===13)
+        this.props.onKeyPressEnter(this.state.todoText)
+    }
 render(){
     return(
         <>
-        <input type="text" placeholder="what needs to be done"/>
+        <input type="text" onChange={this.handleChange} onKeyDown={this.handleKeyDown} placeholder="what needs to be done"/>
         </>
     )
 }
