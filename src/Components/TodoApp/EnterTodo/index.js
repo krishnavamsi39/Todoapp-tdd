dac4a0f0727dfb4b9e6a2f302729291d74f4202f
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-02 12:13:03 +0530

    [Green state] for entertodo component

---
+++ src/Components/TodoApp/EnterTodo/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class EnterTodo extends Component{
+render(){
+    return(
+        <>
+        <input type="text" placeholder="what needs to be done"/>
+        </>
+    )
+}
+}
+export default EnterTodo
