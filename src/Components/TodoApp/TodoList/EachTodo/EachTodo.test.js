63d5f89f220aa491e209925262e94b7cba89e89e
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-03 09:30:25 +0530

    [green state] for eachtodo  editable todo task

--- src/Components/TodoApp/TodoList/EachTodo/EachTodo.test.js
+++ src/Components/TodoApp/TodoList/EachTodo/EachTodo.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import EachTodo from './index'
 import Todo from '../../../../Models/Todo'
 import TodoStore from '../../../../Store/TodoStore'
+
 afterEach(cleanup)
 describe('Test for EachTodo Component',()=>{
     it('should check the checkbox',()=>{
@@ -24,13 +25,28 @@ describe('Test for EachTodo Component',()=>{
         expect(todoStore.todos.length).toBe(0)
     })
     it('should become editable text on double click and text should change on clicking enter key',()=>{
-        const todo=new Todo("mobx",1,false)
+        const todo=new Todo("Mobx",1,false)
         jest.spyOn(todo,'updateTodoText')
-        const {getByTestId}=render(<EachTodo todo={todo}/>)
+        const {getByTestId,getByPlaceholderText}=render(<EachTodo todo={todo}/>)
         const editableText=getByTestId("double-click")
-        fireEvent.dblClick(editableText)
-        
+       fireEvent.doubleClick(editableText)
 
+        const inputTodo=getByPlaceholderText("what needs to be done")
+        expect(inputTodo).toBeDefined()
+        const changeEvent={
+            target:{
+                value:"react"
+            }
+        }
+        fireEvent.change(inputTodo,changeEvent)
+        const keyDownEvent={
+            key:"Enter",
+            code:13,
+            keyCode:13
+        }
+        fireEvent.keyDown(inputTodo,keyDownEvent)
+
+        expect(todo.updateTodoText).toHaveBeenCalledWith("react")
 
     })
 })
--- src/Components/TodoApp/TodoList/EachTodo/index.js
+++ src/Components/TodoApp/TodoList/EachTodo/index.js
@@ -1,16 +1,35 @@
 import React,{Component} from 'react'
+import EnterTodo from '../../EnterTodo';
 class EachTodo extends Component{
+    constructor(props){
+        super(props)
+        this.state={isEditable:false}
+    }
+    handleDoubleClick=()=>{
+        this.setState({isEditable:true})
+    }
     handleClick=()=>{
         this.props.todo.toggleTodo()
     }
     handleDelete=()=>{
         this.props.todoStore.deleteTodo(this.props.todo.id)
     }
+    handleUpdate=(message)=>{
+        this.props.todo.updateTodoText(message)
+    }
+    handleEditableText=()=>{
+        if(!this.state.isEditable){
+            return <span data-testid="double-click" onDoubleClick={this.handleDoubleClick}></span>
+        }
+        else{
+            return <EnterTodo onKeyPressEnter={this.handleUpdate}/>
+        }
+    }
 render(){
     return(
         <>
         <input data-testid="checkbox" type="checkbox" onClick={this.handleClick} />
-        
+        {this.handleEditableText()}
         <button data-testid="delete-todo" onClick={this.handleDelete}/>
         </>
     )
