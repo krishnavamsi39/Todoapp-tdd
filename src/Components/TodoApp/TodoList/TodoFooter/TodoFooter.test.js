ded5a7a54eded8d534f5581fa8439f7bacfda4dc
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-03 10:53:01 +0530

    [refactored state ] for todofooter component active button

--- src/Components/TodoApp/TodoList/TodoFooter/TodoFooter.test.js
+++ src/Components/TodoApp/TodoList/TodoFooter/TodoFooter.test.js
@@ -4,26 +4,26 @@ import TodoFooter from './index'
 import TodoStore from '../../../../Store/TodoStore'
 import todoFilterTypes from '../../../../Constants/todoFilter'
 afterEach(cleanup)
+
 describe('test for todofooter component',()=>{
+    let todoStore
+    beforeEach(()=>{
+        todoStore=new TodoStore()
+            jest.spyOn(todoStore,'changeFilter')       
+    })
     it('should test if filter value is changing',()=>{
-        const todoStore=new TodoStore()
-        jest.spyOn(todoStore,'changeFilter')
         const {getByTestId}=render(<TodoFooter todoStore={todoStore}/>)
         const allbutton=getByTestId(todoFilterTypes.all)
         fireEvent.click(allbutton)
         expect(todoStore.changeFilter).toHaveBeenCalledWith(todoFilterTypes.all)
     })
     it('should test if filter value is changing to completed',()=>{
-        const todoStore=new TodoStore()
-        jest.spyOn(todoStore,'changeFilter')
         const {getByTestId}=render(<TodoFooter todoStore={todoStore}/>)
         const completedbutton=getByTestId(todoFilterTypes.completed)
         fireEvent.click(completedbutton)
         expect(todoStore.changeFilter).toHaveBeenCalledWith(todoFilterTypes.completed)
     })
     it('should test if filter value is changing to active',()=>{
-        const todoStore=new TodoStore()
-        jest.spyOn(todoStore,'changeFilter')
         const {getByTestId}=render(<TodoFooter todoStore={todoStore}/>)
         const activebutton=getByTestId(todoFilterTypes.active)
         fireEvent.click(activebutton)
