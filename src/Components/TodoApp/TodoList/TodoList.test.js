c481b7811d38bd251f2149985d372002c9412662
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-03 09:41:04 +0530

    [red state] for todolist component

--- src/Components/TodoApp/TodoList/TodoList.test.js
+++ src/Components/TodoApp/TodoList/TodoList.test.js
@@ -1,8 +1,15 @@
 import {render,cleanup, fireEvent} from '@testing-library/react'
 import React from 'react'
 import TodoList from './index'
-describe('',()=>{
-    it('',()=>{
+import TodoStore from '../../../Store/TodoStore'
+afterEach(cleanup);
+describe('test for list of todos',()=>{
+    it('should render list of todos',()=>{
+        const todoStore=new TodoStore()
+        todoStore.addTodo("mobx")
+        todoStore.addTodo("react")
+        const {getAllByTestId}=render(<TodoList />)
+
         
     })
 })
--- src/Components/TodoApp/TodoList/index.js
+++ src/Components/TodoApp/TodoList/index.js
@@ -8,4 +8,3 @@ render(){
     )
 }
 }
-export default TodoList
